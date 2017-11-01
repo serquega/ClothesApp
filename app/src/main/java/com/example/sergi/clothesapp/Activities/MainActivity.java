2e21091f422c756291ca80a53a5ba56a4a24031d
Sergi Quevedo <sergi.quevedo@estudiants.urv.cat>
2017-11-01 09:43:56 +0100

    app changes

--- app/src/main/java/com/example/sergi/clothesapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/sergi/clothesapp/Activities/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,6 +21,7 @@ import com.example.sergi.clothesapp.R;
 import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonSignIn;
     private EditText editTextEmail;
     private EditText editTextPassword;
+    private TextView textViewSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,25 +40,43 @@ public class MainActivity extends AppCompatActivity {
         editTextEmail=(EditText) findViewById(R.id.editTextEmail);
         editTextPassword=(EditText) findViewById(R.id.editTextPassword);
         buttonSignIn=(Button) findViewById(R.id.buttonSignIn);
+        textViewSignUp=(TextView) findViewById(R.id.textViewSignup);
+        textViewSignUp.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                startActivity(SignUpActivity.class);
+                return true;
+            }
+        });
         buttonSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 checkData(editTextEmail.getText(), editTextPassword.getText());
-
             }
         });
 
     }
 
-    public void checkData(Editable email, Editable password){
+    public void checkData(Editable eMail, Editable passWord){
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/java/com/example/sergi/clothesapp/Files/data.txt"));
             String line=null;
+            boolean found=false;
             StringTokenizer token;
-            bufferedReader.readLine();
+            line=bufferedReader.readLine();
             while(line!=null){
                 token=new StringTokenizer(line,",");
-                bufferedReader.readLine();
+                String name=token.nextToken();
+                String surname=token.nextToken();
+                String dni=token.nextToken();
+                String email=token.nextToken();
+                String password=token.nextToken();
+
+                if(email.equals(eMail.toString())&& password.equals(passWord.toString())) {
+                    found=true;
+                    //startActivity();
+                }else
+                    line=bufferedReader.readLine();
             }
 
             bufferedReader.close();
