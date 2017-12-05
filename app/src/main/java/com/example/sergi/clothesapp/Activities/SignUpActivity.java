a7b0ce9ca094c77fb781c7297f141f5e3e165e4a
Sergi Quevedo <sergi.quevedo@estudiants.urv.cat>
2017-12-05 16:04:32 +0100

    modifications in sign up activity

--- app/src/main/java/com/example/sergi/clothesapp/Activities/SignUpActivity.java
+++ app/src/main/java/com/example/sergi/clothesapp/Activities/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.sergi.clothesapp.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,10 +12,36 @@ import com.example.sergi.clothesapp.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    FloatingActionButton fabMan;
+    FloatingActionButton fabWoman;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        fabMan=(FloatingActionButton) findViewById(R.id.fabMan);
+        fabWoman=(FloatingActionButton) findViewById(R.id.fabWoman);
+
+        fabMan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(ManActivity.class);
+            }
+        });
+
+        fabWoman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(WomanActivity.class);
+            }
+        });
+    }
+
+    //Method for starting an activity
+    public void startActivity(Class<?> startActivity){
+        Intent intent = new Intent(this, startActivity);
+        startActivity(intent);
     }
 
 }
